c2e7d7feaed544eb43381ad9dc540cec5f7da84d
Charlie <764189@newcollege.ac.uk>
2024-12-13 11:46:08 +0000

    characterselectwithoutplay

---
+++ Assets/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject selectedSkin;
+    public GameObject Player;
+
+    private Sprite PlayerSprite;
+    void Start()
+    {
+        PlayerSprite = selectedSkin.GetComponent<SpriteRenderer>().sprite;
+
+        Player.GetComponent<SpriteRenderer>().sprite = PlayerSprite;
+
+
+    }
+}
