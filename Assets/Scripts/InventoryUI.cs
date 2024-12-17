434a802bdb2a31b4b9ed248a99b2e39273805b41
Charlie <764189@newcollege.ac.uk>
2024-12-17 22:49:12 +0000

    updates

--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -1,18 +1,18 @@
 using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
 
-public class InventoryUI : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+public class toggle : MonoBehaviour
+
+{
+
+   public void whenButtonClicked()
+{
+gameObject.SetActive(!gameObject.activeInHierarchy);
+}
+
 }
